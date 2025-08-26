3db666cfd298ea5a585d64a98b6f554d4f9b4549
ARSHITH BAALA <arshu@LAPTOP-1PEMKRKG.(none)>
2025-08-26 14:27:26 +0530

    third

---
+++ .history/app_20250826142658.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const careerRoutes = require('./routes/career');
+
+app.use(express.json());
+app.use('/api', careerRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+const careerRoutes = require('./routes/career');
+app.use('/api', careerRoutes);
+
